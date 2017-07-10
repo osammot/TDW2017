5dfce29f6087ea721693318a081dd474bb782dd4
AcuLroD <lucaadorazio@gmail.com>
2017-07-10 15:01:41 +0200

    Add files via upload
    modifiche html e css

---
+++ new/index.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+
+
+require("include/dbms.inc.php");
+require("include/template2.inc.php");
+require ("include/varie.php");
+//require("include/auth.inc.php");
+
+$body = new Template("index.html");
+
+$db->query("SELECT prodotti.*, immagini.path FROM prodotti left join immagini on prodotti.id_immaginePrincipale= immagini.id_immagine LIMIT 20"); //per div alcuni dei nostri prodotti
+
+$row = $db->getResult();
+
+foreach($row as $rows) {
+
+    $body->setContent($rows);
+
+} //end foreach
+
+$db->query("SELECT prodotti.*, immagini.path FROM prodotti left join immagini on prodotti.id_immaginePrincipale= immagini.id_immagine WHERE prezzo <= 20.00 LIMIT 3"); //div a prezzi bassi
+
+$row1 = $db->getResult();
+
+foreach($row1 as $rows1) {
+    $varid = $rows1['id_prodotto'];
+    $varnome = $rows1['nome'];
+    $varprezzo = $rows1['prezzo'];
+    $varpath = $rows1['path'];
+
+    $body->setContent("id_prodotto1",$varid);
+    $body->setContent("offertanome",$varnome);
+    $body->setContent("offertaprezzo",$varprezzo);
+    $body->setContent("offertapath",$varpath);
+
+} //end foreach
+
+$db->query("SELECT id_news, immagine, titolo, data_news FROM news ORDER BY data_news DESC LIMIT 3"); //div news
+
+$row3 = $db->getResult();
+
+foreach($row3 as $rows3) {
+
+    $varnews = $rows3['id_news'];
+    $varpath3 = $rows3['immagine'];
+    $vartitolo3 = $rows3['titolo'];
+    $vardata3 = $rows3['data_news'];
+
+
+    $body->setContent("id_news",$varnews);
+    $body->setContent("immagine",$varpath3);
+    $body->setContent("titolo",$vartitolo3);
+    $body->setContent("dataaaaaaaaa",$vardata3);
+
+} //end foreach
+
+$db->query("SELECT prodotti.id_prodotto, prodotti.prezzo, prodotti.titolo, prodotti.data, prodotti.id_immaginePrincipale,immagini.path FROM prodotti left join immagini on prodotti.id_immaginePrincipale= immagini.id_immagine ORDER BY data DESC LIMIT 3"); //div ultimi usciti
+
+$row2 = $db->getResult();
+
+foreach($row2 as $rows2) {
+    $varid2 = $rows2['id_prodotto'];
+    $varnome2 = $rows2['titolo'];
+    $vardata2 = $rows2['data'];
+    $varprezzo2 = $rows2['prezzo'];
+    $varpath2 = $rows2['path'];
+
+    $body->setContent("id_prodotto2",$varid2);
+    $body->setContent("newsnome",$varnome2);
+    $body->setContent("newsdata",$vardata2);
+    $body->setContent("newsprezzo",$varprezzo2);
+    $body->setContent("newspath",$varpath2);
+
+} //end foreach
+
+
+
+
+
+
+$main->setContent("menu",$menu->get());
+$main->setContent("body",$body->get());
+$main->close();
+
+
+?>
